6d52ba9c2645518aeb3f3240c2977fd55e5236cb
Raja Gupta <37621211+rajagupta20@users.noreply.github.com>
2020-06-10 14:08:59 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
 module.exports = function(grunt) {
-            "use strict";
-            grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-            grunt.registerTask("default", [
-                        "clean",
-                        "lint",
-                        "build"
-            ]);
+    "use strict";
+    grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+    grunt.registerTask("default", [
+        "clean",
+        "lint",
+        "build"
+    ]);
+    grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
+    grunt.registerTask("unit_and_integration_tests", ["test"]);
+    grunt.config.merge({
+        coverage_threshold: {
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90
+        }
+    });
 };
